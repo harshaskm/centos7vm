6f567b6675a3f3471da78e9a853f1e0c206d5a76
Harsha Mohan <harshaskm@gmail.com>
2016-09-05 00:09:12 +0800

    Loads of refactoring, more to go, I wish I could have written better comment here, way past mid night, want to go to bed real bad

---
+++ deploy_DBs/postgresqlDB/scripts/destroy_postgresqlOnCentos7.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This is a shell script to destroy the "postgresqlOnCentos7" vm, only if it exists, so that the pipelines won't turn red.
+# This script and others need re-factoring to parameterise vm name so that these could be used for other vms also.
+
+TEMPSTR=`vagrant status | grep "postgresqlOnCentos7" | sed 's/postgresqlOnCentos7//g' | xargs`
+if [ "$TEMPSTR" == 'not created (virtualbox)' ]; then
+  echo 'No such VM to destroy'
+else
+  echo 'destroying vm please wait...'
+  vagrant destroy postgresqlOnCentos7 -f
+fi
+
