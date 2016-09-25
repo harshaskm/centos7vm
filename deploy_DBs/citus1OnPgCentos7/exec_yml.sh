3c27204046ceba37773f3eeedc86d68ddd64f1b5
Harsha Mohan <harshaskm@gmail.com>
2016-09-25 20:37:56 +0800

    Oh so many changes! I was trying different modules in ansible to download and unarchive the citus tutorial package, but it failed, which reason, I have now resorted to get it done through command module. Also, one other major change is to introduce one more hosts file in between to login as user citus1 and do all the things that belong to that user through that user. File name is: citus1_tutorial_hosts and the yml for the same is: citus1_tutorial_playbook.yml

--- deploy_DBs/citus1OnPgCentos7/exec_yml.sh
+++ deploy_DBs/citus1OnPgCentos7/exec_yml.sh
@@ -1,3 +1,5 @@
 cd /Users/hmohan/centos7vm/deploy_DBs/citus1OnPgCentos7
 export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook -i citus1_ansible_hosts citus1_playbook.yml
+ansible-playbook -i citus1_tutorial_hosts citus1_tutorial_playbook.yml
+
